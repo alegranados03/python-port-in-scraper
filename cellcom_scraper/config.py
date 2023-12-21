0810f89f70f261dac65ef3fda941ba43b2fb5a2f
Alejandro <alejandro.granados.dev@gmail.com>
2023-12-21 11:05:15 -0600

    New error added to system

--- cellcom_scraper/config.py
+++ cellcom_scraper/config.py
@@ -7,6 +7,7 @@ FORCE_STOP_ERRORS = [
     "The original order cannot be found for this port-in number. Please verify the information entered and try again.",
     "The status for the mobile number entered is not valid.",
     "The number entered does not meet the criteria for port-in date change.",
-    "The Port in Date change function is not available for previous mobile number change transactions. Please contact Retail Support to complete the transaction."
+    "The Port in Date change function is not available for previous mobile number change transactions. Please contact Retail Support to complete the transaction.",
+    "A Port In request is still in progress and changes cannot be made at this time. Please contact Retail Support to complete the request."
 ]
 AWS_SERVER_URL = os.environ.get("AWS_SERVER_URL")
