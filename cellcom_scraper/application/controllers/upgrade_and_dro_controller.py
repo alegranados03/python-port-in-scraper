778cfef61350879b8f419b8b2d5c539ddd04623e
Alejandro <alejandro.granados.dev@gmail.com>
2025-01-07 10:24:05 -0600

    :recycle: time added

--- cellcom_scraper/application/controllers/upgrade_and_dro_controller.py
+++ cellcom_scraper/application/controllers/upgrade_and_dro_controller.py
@@ -108,5 +108,5 @@ class UpgradeAndDroController(FastActController):
                     transaction.rollback()
                 finally:
                     print(f"{self.__class__.__name__}: Thread FINISHED.")
-                    time.sleep(60)
+                    time.sleep(10)
                     transaction.close()
