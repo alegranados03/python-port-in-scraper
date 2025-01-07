034ce9e342eda00a7d50f3cd4a7e71d62adc8a51
Alejandro <alejandro.granados.dev@gmail.com>
2025-01-07 13:13:59 -0600

    :recycle: merge bad connection configuration in database.py

--- cellcom_scraper/infrastructure/sqlalchemy/database.py
+++ cellcom_scraper/infrastructure/sqlalchemy/database.py
@@ -1,5 +1,4 @@
 import os
-
 from dotenv import load_dotenv
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -15,12 +14,18 @@ def create_connection_string():
     port = os.environ.get("DATABASE_PORT")
     database_name = os.environ.get("DATABASE_NAME")
     connector = os.environ.get("DATABASE_CONNECTION")
+    environment = os.environ.get("ENVIRONMENT", "develop")
+    ssl_cert_path = os.environ.get("DB_SSL_PATH")
+
     match connector:
         case "postgresql":
             return f"postgresql://{user}:{password}@{host}:{port}/{database_name}"
         case "mysql":
-            return f"mysql://{user}:{password}@{host}:{port}/{database_name}"
-
+            match environment:
+                case "develop":
+                    return f"mysql://{user}:{password}@{host}:{port}/{database_name}"
+                case "production":
+                    return f"mysql://{user}:{password}@{host}:{port}/{database_name}?ssl_ca={ssl_cert_path}"
 
 def create_default_engine():
     return create_engine(create_connection_string())
